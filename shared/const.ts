6b79d277747ce77fe33d13a534df0e14f66bd1aa
ryabinkin.d <ryabinkin.d@fasttech.dev>
2023-02-20 00:22:19 +0300

    feat: change deploy server

--- shared/const.ts
+++ shared/const.ts
@@ -3,5 +3,5 @@ export const UNDEFINED_CARD_VALUE = 'UNDEFINED';
 export const MAX_CHAT_ENTRIES = 50;
 
 export const API_URL = IS_PROD
-  ? 'wss://rss-react-2021q3-pp.herokuapp.com/'
+  ? 'https://planning-poker-ookkd0l0i-zmiterr.vercel.app'
   : 'ws://localhost:9000';
